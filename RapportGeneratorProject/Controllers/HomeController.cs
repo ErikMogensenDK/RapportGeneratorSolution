b8c8e32528582d6b970eefb638bad6ab98fcbec5
ErikMogensen <77505542+Ondonden@users.noreply.github.com>
2023-07-03 10:16:45 +0200

    Add project files.

---
+++ RapportGeneratorProject/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RapportGeneratorProject.Models;
+using System.Diagnostics;
+
+namespace RapportGeneratorProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitForm(int hik, int vfi)
+        {
+            // Generate rapport using input
+            // upload document
+            // change to page where you can download rapport
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RapportGeneratorProject/Views/Home/GeneratedRapport.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RapportGeneratorProject.Views.Home
+{
+    public class GeneratedRapportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
